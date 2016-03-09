ab18c6e9534f9fc812437cfd6bf1bf9fee0e3df5
ikeloo <ikeloo2718@gmail.com>
2016-03-09 17:28:21 +0800

    develop commit

--- src/com/cipher/test/CipherTest.java
+++ src/com/cipher/test/CipherTest.java
@@ -14,7 +14,7 @@ import com.cipher.util.RSAJavaToCSharp;
 import com.cipher.util.TranscodeUtil;
 
 public class CipherTest {
-
+	// develop change
     String rsaPrivateKey;
     String rsaPublicKey;
     String aesKeyClient;
