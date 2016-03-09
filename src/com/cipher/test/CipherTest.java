0788e4a682bd3f1769996b94fc9e7dc00649d7a5
ikeloo <ikeloo@126.com>
2016-03-09 17:41:16 +0800

    change from mac

--- src/com/cipher/test/CipherTest.java
+++ src/com/cipher/test/CipherTest.java
@@ -14,7 +14,7 @@ import com.cipher.util.RSAJavaToCSharp;
 import com.cipher.util.TranscodeUtil;
 
 public class CipherTest {
-	// develop change
+	// develop change mac
     String rsaPrivateKey;
     String rsaPublicKey;
     String aesKeyClient;
